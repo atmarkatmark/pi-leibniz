d1ba567b65b9392f9221af99d60f6c29521f0a74
atmark <atmarkatmark@gmail.com>
2019-05-12 18:54:44 +0900

    Added source written in C

---
+++ c/leibniz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+double leibniz(unsigned long long n)
+{
+	double sum = 0;
+
+	/* iが偶数 */
+	for (unsigned long long i = 0; i < n; i += 2)
+		sum += 1.0 / (2 * i + 1);
+	
+	/* iが奇数 */
+	for (unsigned long long i = 1; i < n; i += 2)
+		sum += -1.0 / (2 * i + 1);
+	
+	return sum;
+}
+
+int main(int argc, char *argv[])
+{
+	clock_t start;
+	int digit = 1;
+
+	/* 桁数が指定されているか確認 */
+	if (argc < 2)
+	{
+		puts("Usage: %s NUMBER_OF_DIGITS");
+		return -1;
+	}
+
+	/* パラメータから桁数を取得 */
+	digit = atoi(argv[1]);
+
+	/* Leibnizの公式を計算 */
+	start = clock();
+	printf("pi: %.12lf\n", 4 * leibniz((unsigned long long )pow(10, digit)));
+	printf("time: %.8lf\n", (clock() - start) / CLOCKS_PER_SEC);
+	
+	return 0;
+}
